8e8917169a96ab6995beeb1722b4c74b1f1afbcc
aewens <email@aewens.com>
2018-12-01 08:32:14 -0600

    Day 1, Part 2 solution

--- day1/freq.py
+++ day1/freq.py
@@ -8,4 +8,25 @@ with open("input", "r") as puzzle:
 for change in changes:
     curr_frequency = curr_frequency + change
 
-print(curr_frequency)
+print("Part 1:", curr_frequency)
+
+curr_frequency = 0
+found = list()
+repeated = None
+iteration = 0
+
+while repeated is None:
+    for change in changes:
+        iteration = iteration + 1
+        curr_frequency = curr_frequency + change
+        # print("%s\t%s\t%s" % (
+        #     curr_frequency in found, curr_frequency, iteration))
+        if curr_frequency in found:
+            repeated = curr_frequency
+            break
+        else:
+            found.append(curr_frequency)
+
+print("Part 2:", repeated)
+
+
